fbd3fa4da3b5ecd806b1bd935395b5f97ec92202
Alexander Díaz Márquez <adiazm@admsys.cl>
2023-08-21 17:23:11 -0400

    se actualiza el nombre del factory

--- database/factories/CommentFactory.php
+++ database/factories/CommentFactory.php
@@ -1,14 +1,16 @@
 <?php
 
 namespace Admsys\Comments\Database\Factories;
-
-use Admsys\Comments\Models\Comment;
 use Illuminate\Database\Eloquent\Factories\Factory;
 
 class CommentFactory extends Factory
 {
-    protected $model = Comment::class;
-
+    //protected $model = Comment::class;
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
     public function definition()
     {
         return [
